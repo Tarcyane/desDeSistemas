4c7725abe694bd7a7455d8cf63626e60b6bf3d8e
Tarcyane <cardosotarcy6@gmail.com>
2025-05-12 12:23:43 -0300

    Add files via upload

---
+++ revisao.py
@@ -0,0 +1,13 @@
+nome:Tarcyane Cardoso Anjos
+numero_chamada:24
+
+print("Olá! Seja bem-vindo ao sistema. Este código foi desenvolvido por" ,nome"número da chamada"numero_chamada".")
+
+
+
+altura: 1,53
+livro_favorito:dellvis nit
+comida_favorito:strogonoff
+musicafavorito:Seven 
+filme_favorito:Pra todos os garotos que já amei
+
